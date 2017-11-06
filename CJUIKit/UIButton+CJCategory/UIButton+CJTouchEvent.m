e1240c6cbf5e57f8903eee793b365dda411018a2
dvlproad <studyroad@qq.com>
2017-11-06 18:30:51 +0800

    【Feature】UIButton

---
+++ CJUIKit/UIButton+CJCategory/UIButton+CJTouchEvent.m
@@ -0,0 +1,34 @@
+//
+//  UIButton+CJTouchEvent.m
+//  CJUIKitDemo
+//
+//  Created by ciyouzen on 7/9/15.
+//  Copyright (c) 2015 dvlproad. All rights reserved.
+//
+
+#import "UIButton+CJTouchEvent.h"
+#import <objc/runtime.h>
+
+@implementation UIButton (CJTouchEvent)
+
+#pragma mark - runtime
+static NSString *cjTouchEventBlockKey = @"cjTouchEventBlockKey";
+
+- (void (^)(UIButton *))cjTouchEventBlock {
+    return objc_getAssociatedObject(self, (__bridge const void *)(cjTouchEventBlockKey));
+}
+
+- (void)setCjTouchEventBlock:(void (^)(UIButton *))cjTouchEventBlock {
+    objc_setAssociatedObject(self, (__bridge const void *)(cjTouchEventBlockKey), cjTouchEventBlock, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    
+    [self addTarget:self action:@selector(cjTouchEventAction:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+// 按钮点击
+- (void)cjTouchEventAction:(UIButton *)button {
+    if (self.cjTouchEventBlock) {
+        self.cjTouchEventBlock(button);
+    }
+}
+
+@end
---
+++ CJUIKit/UIButton+CJCategory/UIButtonCJCategory.h
@@ -0,0 +1,16 @@
+//
+//  UIButtonCJCategory.h
+//  CJUIKitDemo
+//
+//  Created by ciyouzen on 7/9/15.
+//  Copyright (c) 2015 dvlproad. All rights reserved.
+//
+
+#ifndef UIButtonCJCategory_h
+#define UIButtonCJCategory_h
+
+#import "UIButton+CJTouchEvent.h"
+#import "UIButton+CJMoreProperty.h"
+#import "UIButton+CJUpDownStructure.h"
+
+#endif /* UIButtonCJCategory_h */
