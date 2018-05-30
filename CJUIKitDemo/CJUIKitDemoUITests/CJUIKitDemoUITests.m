615c4fca210455e82d08cbc327bf148a602969fe
dvlproad <studyroad@qq.com>
2018-05-31 02:10:22 +0800

    【Feature】自动化测试浅尝

--- CJUIKitDemo/CJUIKitDemoUITests/CJUIKitDemoUITests.m
+++ CJUIKitDemo/CJUIKitDemoUITests/CJUIKitDemoUITests.m
@@ -35,6 +35,48 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElementQuery *tabBarsQuery = app.tabBars;
+    
+    /*
+    XCUIElement *homeButton = tabBarsQuery.buttons[@"主页"];
+    [homeButton tap];
+    
+    XCUIElement *scrollViewButton = tabBarsQuery.buttons[@"滚动视图"];
+    [scrollViewButton tap];
+    
+    XCUIElement *smallViewButton = tabBarsQuery.buttons[@"基础小视图"];
+    [smallViewButton tap];
+    
+    XCUIElement *cjfoundataionButton = tabBarsQuery.buttons[@"CJFoundataion"];
+    [cjfoundataionButton tap];
+    */
+    XCUIElement *cjutilButton = tabBarsQuery.buttons[@"CJUtil"];
+    [cjutilButton tap];
+    
+    
+    [app.tables/*@START_MENU_TOKEN@*/.staticTexts[@"DataUtil"]/*[[".cells.staticTexts[@\"DataUtil\"]",".staticTexts[@\"DataUtil\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
+    [app.buttons[@"转化上面的文字为拼音"] tap];
+    
+    
+    
+    
+    
+}
+
+
+- (void)testAlert {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.tabBars.buttons[@"CJUtil"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    [tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[@"Alert"]/*[[".cells.staticTexts[@\"Alert\"]",".staticTexts[@\"Alert\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
+    [tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[@"FlagImage & Title & Message & OK & Cancel"]/*[[".cells.staticTexts[@\"FlagImage & Title & Message & OK & Cancel\"]",".staticTexts[@\"FlagImage & Title & Message & OK & Cancel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
+    [app.buttons[@"允许上车"] tap];
+    
+    sleep(2);
+    [app.buttons[@"确认"] tap];
 }
 
 @end
