a93abd1aea8c7399393e8fdaaa9d68bc2338457e
dvlproad <dvlproad@163.com>
2021-01-09 18:28:40 +0800

    【Feature】整理文件目录结构

--- CJBaseUIKit/UITextInputCJHelper/TextField/CQTextFieldDelegate.h
+++ CJBaseUIKit/UITextInputCJHelper/TextField/CQTextFieldDelegate.h
@@ -23,7 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
  *  设置最大长度，并在已封装shouldChange中增加额外的能否输入的判断（如输入手机号码的时候，希望会系统处理出的新文本判断，在新文本不合法的时候能有对应toast提示）
  *
  *  @param maxTextLength                最大长度（英文长度算1，中文长度算2）
- *  @param extraShouldChangeCheckBlock  增加的额外能否输入的判断
+ *  @param extraShouldChangeCheckBlock  增加的额外能否输入的判断（这里添加的block一般都不应该再做长度限制了）
  */
 - (void)setupMaxTextLength:(NSInteger)maxTextLength addExtraShouldChangeCheckBlock:(BOOL (^ _Nullable)(NSString *newText))extraShouldChangeCheckBlock;
 
--- CJBaseUIKit/UITextInputCJHelper/TextField/CQTextFieldDelegate.m
+++ CJBaseUIKit/UITextInputCJHelper/TextField/CQTextFieldDelegate.m
@@ -30,7 +30,7 @@
  *  设置最大长度，并在已封装shouldChange中增加额外的能否输入的判断（如输入手机号码的时候，希望会系统处理出的新文本判断，在新文本不合法的时候能有对应toast提示）
  *
  *  @param maxTextLength                最大长度（英文长度算1，中文长度算2）
- *  @param extraShouldChangeCheckBlock  增加的额外能否输入的判断
+ *  @param extraShouldChangeCheckBlock  增加的额外能否输入的判断（这里添加的block一般都不应该再做长度限制了）
  */
 - (void)setupMaxTextLength:(NSInteger)maxTextLength addExtraShouldChangeCheckBlock:(BOOL (^ _Nullable)(NSString *newText))extraShouldChangeCheckBlock {
     _maxTextLength = maxTextLength;
@@ -52,13 +52,15 @@
     NSString *oldText = textField.text;
     NSString *newText = [oldText stringByReplacingCharactersInRange:range withString:string];//若允许改变，则会改变成的新文本
     //NSLog(@"oldText = %@, newText = %@, range = %@, string = %@", oldText, newText, NSStringFromRange(range), string);
-    if (newText.length < oldText.length) {
-        //NSLog(@"textField deleting...from %zd to %zd", oldText.length, newText.length);
-        return YES;
-    }
-    if (self.maxTextLength != 0 && newText.length > self.maxTextLength+10) { // 有限制，且超过最大值+10的弹性限制(因为非高亮的文本长度偏长，且有时候打一个字可能需要输入几个字符才会有)，则禁止输入
-        return NO;
-    }
+//    if (newText.length < oldText.length) {
+//        //NSLog(@"textField deleting...from %zd to %zd", oldText.length, newText.length);
+//        return YES;
+//    }
+//    if (self.maxTextLength != 0 && range.length == 0 && newText.length > self.maxTextLength+10) {
+//        // 待修复：目前无法知道是输入，还是粘贴。range.length == 0 如果是没有替换文本的插入或者粘贴的话，为避免输入太多，增加体验，额外增加弹性输入限制。
+//        // 有限制，且超过最大值+10的弹性限制(因为非高亮的文本长度偏长，且有时候打一个字可能需要输入几个字符才会有)，则禁止输入
+//        return NO;
+//    }
     /// 以上为额外增加的判断 ///
     
     if (self.extraShouldChangeCheckBlock) {
