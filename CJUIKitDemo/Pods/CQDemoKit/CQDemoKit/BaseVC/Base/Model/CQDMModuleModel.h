1d5297e13554c09cbade157599a69ce65a64d62d
dvlproad <dvlproad@163.com>
2021-03-11 02:35:50 +0800

    【Feature】CJModuleModel

--- CJUIKitDemo/Pods/CQDemoKit/CQDemoKit/BaseVC/Base/Model/CQDMModuleModel.h
+++ CJUIKitDemo/Pods/CQDemoKit/CQDemoKit/BaseVC/Base/Model/CQDMModuleModel.h
@@ -23,6 +23,8 @@
 @property (nonatomic, assign) Class classEntry;             /**< 点击后进入的控制器 */
 @property (nonatomic, assign) BOOL isCreateByXib;           /**< 控制器是否要由interface来生成 */
 @property (nullable, nonatomic, strong) NSBundle *xibBundle;/**< 控制器由interface生成时候所在的bundle（默认nil，表示是xib的时候，该xib在mainBundle中） */
+@property (nonatomic, copy) NSString *xibBundleName;        /**< bundle名字(xibBundle为nil时候，才会使用这个值) */
+
 //②③事件
 @property (nonatomic, assign) SEL   selector;           /**< 点击后执行的方法 */
 @property (nonatomic, copy) void(^actionBlock)(void);   /**< 点击后执行的方法 */
--- CJUIKitDemo/Pods/CQDemoKit/CQDemoKit/BaseVC/Base/Model/CQDMModuleModel.m
+++ CJUIKitDemo/Pods/CQDemoKit/CQDemoKit/BaseVC/Base/Model/CQDMModuleModel.m
@@ -10,5 +10,17 @@
 
 @implementation CQDMModuleModel
 
+- (NSBundle *)xibBundle {
+    NSBundle *xibBundle = _xibBundle;
+    if (xibBundle == nil) {
+        Class classEntry = self.classEntry;
+        NSString *clsString = NSStringFromClass(classEntry);
+        NSBundle *bundle = [NSBundle bundleForClass:clsString];
+        NSString *bundleName = self.xibBundleName;
+        NSURL *url = [bundle URLForResource:bundleName withExtension:@"bundle"];
+        xibBundle = [NSBundle bundleWithURL:url];
+    }
+    return xibBundle;
+}
 
 @end
