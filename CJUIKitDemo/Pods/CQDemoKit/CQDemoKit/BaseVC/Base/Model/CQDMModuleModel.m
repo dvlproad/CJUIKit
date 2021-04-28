d6cd9bd8da673627a621a95039970bc80d48bd21
dvlproad <dvlproad@163.com>
2021-04-29 02:52:03 +0800

    【Feature】update

--- CJUIKitDemo/Pods/CQDemoKit/CQDemoKit/BaseVC/Base/Model/CQDMModuleModel.m
+++ CJUIKitDemo/Pods/CQDemoKit/CQDemoKit/BaseVC/Base/Model/CQDMModuleModel.m
@@ -14,8 +14,8 @@
     NSBundle *xibBundle = _xibBundle;
     if (xibBundle == nil && self.xibBundleName != nil) {
         Class classEntry = self.classEntry;
-        NSString *clsString = NSStringFromClass(classEntry);
-        NSBundle *bundle = [NSBundle bundleForClass:clsString];
+        //NSString *clsString = NSStringFromClass(classEntry);
+        NSBundle *bundle = [NSBundle bundleForClass:classEntry];
         NSString *bundleName = self.xibBundleName;
         NSURL *url = [bundle URLForResource:bundleName withExtension:@"bundle"];
         xibBundle = [NSBundle bundleWithURL:url];
