52ada87f27f3b4f7b2c8d938e783329e365d4250
dvlproad <dvlproad@163.com>
2026-05-12 06:22:50 +0800

    【Fix】AFNetworking

--- CJUIKitDemo/Pods/AFNetworking/AFNetworking/AFNetworkReachabilityManager.m
+++ CJUIKitDemo/Pods/AFNetworking/AFNetworking/AFNetworkReachabilityManager.m
@@ -23,7 +23,7 @@
 #if !TARGET_OS_WATCH
 
 #import <netinet/in.h>
-#import <netinet6/in6.h>
+//#import <netinet6/in6.h>
 #import <arpa/inet.h>
 #import <ifaddrs.h>
 #import <netdb.h>
--- CJUIKitDemo/Pods/YYText/YYText/Component/YYTextLayout.m
+++ CJUIKitDemo/Pods/YYText/YYText/Component/YYTextLayout.m
@@ -1503,19 +1503,19 @@ fail:
         return [YYTextPosition positionWithOffset:position affinity:YYTextAffinityBackward];
     }
     
-    [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
+    [self _insideComposedCharacterSequences:line position:position block:^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
         if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
+            position = fabs(left - point.y) < fabs(right - point.y) ? prev : next;
         } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+            position = fabs(left - point.x) < fabs(right - point.x) ? prev : next;
         }
     }];
-    
-    [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
+
+    [self _insideEmoji:line position:position block:^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
         if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
+            position = fabs(left - point.y) < fabs(right - point.y) ? prev : next;
         } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
+            position = fabs(left - point.x) < fabs(right - point.x) ? prev : next;
         }
     }];
     
